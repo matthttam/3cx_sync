31dc6c43f4770975a69398e6717689fc1805433d
Matt <matt.henry@owensboro.kyschools.us>
2024-04-15 16:39:51 -0500

    Remove old factory code. Remove old tests.

--- tcx_api/tests/util/test_util.py
+++ tcx_api/tests/util/test_util.py
@@ -1,4 +1,3 @@
-from sync.factories.user_entity_factory import UserEntityFactory
 from tcx_api.components.schemas.pbx.schedule import Schedule
 from tcx_api.components.schemas.pbx.enums import RuleHoursType
 import pytest
@@ -21,20 +20,6 @@ class TestTcxStrEnum:
 
         assert test_enum["NONE"] == "None"
         assert test_enum["None"] == "None"
-
-    def test_create_user_entity_breaktime(self):
-        user_data = {
-            "Id": 1,
-            "AuthID": "12345",
-            "Enabled": True,
-            "BreakTime": {"Type": RuleHoursType.BreakTime},
-        }
-
-        user = UserEntityFactory.create_user(**user_data)
-
-        assert user.AuthID == user_data["AuthID"]
-        assert user.Enabled == user_data["Enabled"]
-        assert isinstance(user.BreakTime, Schedule)
 
 
 if __name__ == "__main__":
--- tcx_api/util.py
+++ tcx_api/util.py
@@ -1,5 +1,4 @@
 from enum import StrEnum, EnumMeta
-from typing import Any, Callable, TypeVar
 
 
 class TcxStrEnumMeta(EnumMeta):
