48d470925da3d70f2a0b944efe1c87bc8c21d95c
Matt Henry <matt.henry@owensboro.kyschools.us>
2025-01-13 19:34:53 -0600

    Wrote tests for SyncCSV

--- sync/sync_strategy.py
+++ sync/sync_strategy.py
@@ -42,28 +42,45 @@ class SyncCSV(SyncSourceStrategy):
 
     def initialize(self):
         self.logger.log(LogLevel.INFO, "Initializing CSV Source")
+        self._load_csv_mapping()
+        self._set_comparison_properties()
+
+    def _load_csv_mapping(self):
         self.logger.log(LogLevel.INFO, "Loading CSV Mapping")
         self.mapping = CSVMapping(
             mapping_file_path=initialize_or_get_user_config_file("3cx_sync", "3cx_sync", "conf", "csv_mapping.json"))
         self.mapping.initialize()
-        CSVUser.set_comparison_properties(self.mapping.get("Extension", {}).get("Update", []))
-
         self.logger.log(LogLevel.INFO, "CSV Mapping Loaded")
 
+    def _set_comparison_properties(self):
+        CSVUser.set_comparison_properties(self.mapping.get("Extension", {}).get("Update", []))
+        self.logger.log(LogLevel.INFO, "Comparison Properties Set")
+
     def get_source_users(self) -> Optional[List[User]]:
-        user_data = list()
         self.logger.log(LogLevel.INFO, "Loading CSV User Data")
+        csv_data_path = self._get_csv_data_path()
+        user_data = self._parse_csv_file(csv_data_path)
+        csv_user_list = self._validate_csv_users(user_data)
+        self.logger.log(LogLevel.INFO, f"Loaded {len(csv_user_list)} Users from CSV File"       )
+        return csv_user_list
+
+    def _get_csv_data_path(self) -> str:
+        """Retrieve and validate the CSV data file path."""
         csv_data_path = self.mapping.get("Extension", {}).get("Path", "")
         if not os.path.isfile(csv_data_path):
             self.logger.log(
-                LogLevel.INFO, f"Unable to find file at: {csv_data_path}"
-                )
-            raise (FileNotFoundError)
+                LogLevel.ERROR, f"Unable to find file at: {csv_data_path}"
+            )
+            raise FileNotFoundError(f"CSV file not found at: {csv_data_path}")
+        return csv_data_path
 
+    def _parse_csv_file(self, csv_data_path: str) -> List[dict]:
+        """Parse the CSV file and return a list of user dictionaries."""
+        user_data = []
         with open(csv_data_path) as csv_file:
             csv_reader = csv.reader(csv_file)
-            user_mapping = self.mapping.get("Extension").get("New")
             headers = next(csv_reader)
+            user_mapping = self.mapping.get("Extension").get("New", {})
 
             for row in csv_reader:
                 row_dict = dict(zip(headers, row))
@@ -72,14 +89,14 @@ class SyncCSV(SyncSourceStrategy):
                     for key, value in user_mapping.items()
                     if value in row_dict
                 }
-                if user_dict["Enabled"] == "0":
+                if user_dict.get("Enabled") == "0":
                     user_dict["HotdeskingAssignment"] = ""
                 user_data.append(user_dict)
-        csv_user_list = TypeAdapter(List[CSVUser]).validate_python(user_data)
-        self.logger.log(
-            LogLevel.INFO, f"Loaded {len(csv_user_list)} Users from CSV File"
-            )
-        return csv_user_list
+        return user_data
+
+    def _validate_csv_users(self, user_data: List[dict]) -> List[CSVUser]:
+        """Validate and convert raw user data into a list of CSVUser objects."""
+        return TypeAdapter(List[CSVUser]).validate_python(user_data)
 
     def get_user_update_fields(self) -> list:
         return self.mapping["Extension"]["Update"]
---
+++ tests/sync/test_sync_strategy.py
@@ -0,0 +1,115 @@
+from abc import ABC
+from sync.sync_strategy import SyncSourceStrategy, SyncCSV
+from sync.logging import SyncLogger, LogLevel
+from unittest.mock import MagicMock, patch, mock_open
+import pytest
+
+
+class TestSyncSourceStrategy:
+
+    def test_is_abc(self):
+        assert issubclass(SyncSourceStrategy, ABC)
+
+
+class TestSyncCSV:
+    @pytest.fixture
+    def mock_logger(self):
+        return MagicMock(spec=SyncLogger)
+
+    @pytest.fixture
+    def sync_csv(self, mock_logger):
+        return SyncCSV(logger=mock_logger)
+
+    def test_initialize(self, sync_csv, mock_logger):
+        with patch.object(sync_csv, '_load_csv_mapping') as mock_load_csv_mapping, \
+                patch.object(sync_csv, '_set_comparison_properties') as mock_set_comparison_properties:
+            sync_csv.initialize()
+            mock_load_csv_mapping.assert_called_once()
+            mock_set_comparison_properties.assert_called_once()
+            mock_logger.log.assert_any_call(LogLevel.INFO, "Initializing CSV Source")
+
+    def test_load_csv_mapping(self, sync_csv, mock_logger):
+        with patch('sync.sync_strategy.initialize_or_get_user_config_file', return_value='path/to/mapping.json'), \
+                patch('sync.sync_strategy.CSVMapping') as mock_csv_mapping:
+            sync_csv._load_csv_mapping()
+            mock_csv_mapping.assert_called_once_with(mapping_file_path='path/to/mapping.json')
+            mock_csv_mapping.return_value.initialize.assert_called_once()
+            mock_logger.log.assert_any_call(LogLevel.INFO, "Loading CSV Mapping")
+            mock_logger.log.assert_any_call(LogLevel.INFO, "CSV Mapping Loaded")
+
+    def test_set_comparison_properties(self, sync_csv, mock_logger):
+        with patch('sync.sync_strategy.CSVUser.set_comparison_properties') as mock_set_comparison_properties:
+            sync_csv.mapping = {"Extension": {"Update": ["field1", "field2"]}}
+            sync_csv._set_comparison_properties()
+            mock_set_comparison_properties.assert_called_once_with(["field1", "field2"])
+            mock_logger.log.assert_any_call(LogLevel.INFO, "Comparison Properties Set")
+
+    def test_get_source_users(self, sync_csv, mock_logger):
+        with patch.object(sync_csv, '_get_csv_data_path', 
+                          return_value='path/to/csv_file.csv') as mock_get_csv_data_path, \
+                patch.object(sync_csv, '_parse_csv_file', 
+                             return_value=[{'field1': 'value1', 'field2': 'value2'}]) as mock_parse_csv_file, \
+                patch.object(sync_csv, '_validate_csv_users', 
+                             return_value=['validated_user']) as mock_validate_csv_users:
+            result = sync_csv.get_source_users()
+            mock_get_csv_data_path.assert_called_once()
+            mock_parse_csv_file.assert_called_once_with('path/to/csv_file.csv')
+            mock_validate_csv_users.assert_called_once_with([{'field1': 'value1', 'field2': 'value2'}])
+            mock_logger.log.assert_any_call(LogLevel.INFO, "Loading CSV User Data")
+            mock_logger.log.assert_any_call(LogLevel.INFO, "Loaded 1 Users from CSV File")
+            assert result == ['validated_user']
+
+    def test_get_source_users_file_not_found(self, sync_csv, mock_logger):
+        sync_csv.mapping = {"Extension": {"Path": "non_existent_file.csv"}}
+        with pytest.raises(FileNotFoundError):
+            sync_csv.get_source_users()
+        mock_logger.log.assert_any_call(LogLevel.INFO, "Loading CSV User Data")
+        mock_logger.log.assert_any_call(LogLevel.ERROR, "Unable to find file at: non_existent_file.csv")
+
+    def test_get_csv_data_path(self, sync_csv):
+        with patch('os.path.isfile', return_value=True):
+            sync_csv.mapping = {"Extension": {"Path": "path/to/csv_file.csv"}}
+            assert sync_csv._get_csv_data_path() == "path/to/csv_file.csv"
+
+    def test_get_csv_data_path_file_not_found(self, sync_csv, mock_logger):
+        with patch('os.path.isfile', return_value=False):
+            sync_csv.mapping = {"Extension": {"Path": "non_existent_file.csv"}}
+            with pytest.raises(FileNotFoundError):
+                sync_csv._get_csv_data_path()
+            mock_logger.log.assert_any_call(LogLevel.ERROR, "Unable to find file at: non_existent_file.csv")
+
+    def test_parse_csv_file(self, sync_csv):
+        csv_content = "header1,header2\nvalue1,value2\nvalue3,value4"
+        with patch('builtins.open', new_callable=mock_open, read_data=csv_content):
+            sync_csv.mapping = {"Extension": {"New": {"field1": "header1", "field2": "header2"}}}
+            result = sync_csv._parse_csv_file("path/to/csv_file.csv")
+            expected = [
+                {"field1": "value1", "field2": "value2"},
+                {"field1": "value3", "field2": "value4"}
+            ]
+            assert result == expected
+
+    def test_parse_csv_file_with_disabled_user(self, sync_csv):
+        csv_content = "header1,header2,Enabled\nvalue1,value2,0\nvalue3,value4,1"
+        with patch('builtins.open', new_callable=mock_open, read_data=csv_content):
+            sync_csv.mapping = {"Extension": {"New": {"field1": "header1", "field2": "header2", "Enabled": "Enabled"}}}
+            result = sync_csv._parse_csv_file("path/to/csv_file.csv")
+            expected = [
+                {"field1": "value1", "field2": "value2", "Enabled": "0", "HotdeskingAssignment": ""},
+                {"field1": "value3", "field2": "value4", "Enabled": "1"}
+            ]
+            assert result == expected
+
+    def test_validate_csv_users(self, sync_csv):
+        user_data = [{"field1": "value1", "field2": "value2"}]
+        with patch('sync.sync_strategy.TypeAdapter.validate_python', return_value=['validated_user']) as mock_validate:
+            result = sync_csv._validate_csv_users(user_data)
+            mock_validate.assert_called_once_with(user_data)
+            assert result == ['validated_user']
+
+    def test_get_user_update_fields(self, sync_csv):
+        sync_csv.mapping = {"Extension": {"Update": ["field1", "field2"]}}
+        assert sync_csv.get_user_update_fields() == ["field1", "field2"]
+
+    def test_get_source_groups(self, sync_csv):
+        assert sync_csv.get_source_groups() is None
