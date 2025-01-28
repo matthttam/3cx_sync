7f357ece8138dd8d506686fa2b2e0f432a5cb530
Matt Henry <matt.henry@owensboro.kyschools.us>
2025-01-27 21:45:58 -0600

    remove extracted module

--- threecxapi/components/response.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class Response(BaseModel):
-    context: str = Field(None, alias="@odata.context")
-    count: int = Field(None, alias="@odata.count")
--- threecxapi/tests/components/test_parameters.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC
-from threecxapi.components.parameters import ListParameters
-
-
-class TestParameters:
-
-    def test_parameters_is_abc(self):
-        assert issubclass(ListParameters, ABC)
